bb6e271be29629d7f3fc8c101f628d2b502b0575
FabriDamazio <fabridamazio@gmail.com>
2022-08-04 20:12:16 -0300

    Add Application.ex

---
+++ test/makeup/lexers/markdown_lexer_test.exs
@@ -0,0 +1,17 @@
+defmodule Makeup.Lexers.MarkdownLexerTest do
+  use ExUnit.Case, async: true
+
+  alias Makeup.Registry
+  alias Makeup.Lexers.MarkdownLexer
+  alias Makeup.Lexer.Postprocess
+
+  describe "registration" do
+    test "fetching the lexer by name" do
+      assert {:ok, {MarkdownLexer, []}} == Registry.fetch_lexer_by_name("markdown")
+    end
+
+    test "fetching the lexer by extension" do
+      assert {:ok, {MarkdownLexer, []}} == Registry.fetch_lexer_by_extension("md")
+    end
+  end
+end
