26e46a54b9f746e14388e7728713f3d12dfeef86
FabriDamazio <fabridamazio@gmail.com>
2022-08-06 14:38:19 -0300

    Remove comments from mix.exs

--- mix.exs
+++ mix.exs
@@ -21,7 +21,6 @@ defmodule MakeupMarkdown.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger],
@@ -29,11 +28,9 @@ defmodule MakeupMarkdown.MixProject do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:makeup, "~> 1.0"}
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 
@@ -52,7 +49,6 @@ defmodule MakeupMarkdown.MixProject do
     ]
   end
 
-  # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 end
